f2706a3b43a2cab5048c292aca151c61a96951ad
Heqiao Liu <heqiaoliu@gmail.com>
2014-06-18 15:07:37 -0700

    experimental module of pm

--- view/module-pm.js
+++ view/module-pm.js
@@ -6,34 +6,57 @@ var FileExplore=(function($){
 	/**
 	 * Private properties.
 	 */
-	var container=
+	$container=
 	{
 		body:null,
-		buttons:null
+		bacteria:null
+		
 	},
-	FE=
+	// The object to store received data from api.
+	buffer=
 	{
-		BACTERIA:"<span>{{bact.%BACT-NAME%}}</span>",
-		PLATE:"<span>{{plate.%PLATE-NAME%}}</span>",
-		EXP:"<span>{{plate.%EXP-NAME%}}</span>"
-	}
-	,
-	init=function(){
-
+		bacteria:{},
+		experiment:{}
+	},
+	//HTML template strings.
+	template=
+	{
+		BLANK:"<%TYPE% class='%CLASS%'>%NAME%</%TYPE%>",
+		bacteria:"<div>%NAME%</div>"
 	};
+
+	String.prototype.setName=function(name){
+		return this.replace("%NAME%",name);
+	}
+	
+	function appendBacteria(dataobj){
+		if (dataobj.success=="False")
+			return;
+		var data=dataobj.data;
+		for (var x in data){
+			var temp=data[x].bacteria_external_id;
+			buffer.bacteria[temp]=data[x];
+			$container.body.append(template.bacteria.setName(temp));
+		}
+		$container.bacteria=$container.body.children();
+	}
+
+	function appendExperiment(dataobj){
+	
+	}
 	
 	/**
 	 * jQuery properties.
 	 * 
 	 */
-	$.fn.FileExplore=function(){
-		container.body=this;
-	}
 
 	/**
 	 * public functions.
 	 */
 	return {
-
+		init: function(elem){
+			$container.body=$(elem);
+			pmapiClient.getBacteriaList("0",appendBacteria);
+		}
 	}
-})(jQuery)
+})(jQuery);
